bd5e79e0e3bdc8be9bd5a42f2b7155e171b365bf
ishanrahane <ishan1306@gmail.com>
2024-05-06 11:51:26 +0530

    added count of chars

--- Java_8/April_25_Array_String_Java_8/SCountFrequency.java
+++ Java_8/April_25_Array_String_Java_8/SCountFrequency.java
@@ -1,7 +1,17 @@
 package April_25_Array_String_Java_8;
 
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class SCountFrequency {
     public static void main(String[] args) {
+        String str = "Ishan Rahane";
 
+        Map<Character, Long> charCount = str.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        charCount.forEach((character, count) -> System.out.println(character + ": " + count));
     }
 }
